d741c587ff9a47575b223720e1909653914bbf82
Bella Aprillia H <bellaprilliah@gmail.com>
2019-10-07 22:50:53 +0700

    Membuat dan mengatur controller

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+
+class About {
+	public function index($nama = 'Bella', $pekerjaan ='Fangirl')
+	{
+		echo "Hai, nama saya $nama, saya seorang $pekerjaan";
+	}
+	public function page()
+	{
+		echo 'About/page';
+	}
+}
