5172cf21dcf6678057b5abdb0295736355a5ca7c
Bella Aprillia H <bellaprilliah@gmail.com>
2019-10-09 17:54:54 +0700

    Menggunakan database Wrapper

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -1,12 +1,21 @@
-<?php
+<?php 
 
 class Mahasiswa extends Controller {
-	public function index()
-	{
+	public function index() {
 		$data['judul'] = 'Daftar Mahasiswa';
 		$data['mhs'] = $this->model('Mahasiswa_model')->getAllMahasiswa();
 		$this->view('templates/header', $data);
 		$this->view('mahasiswa/index', $data);
 		$this->view('templates/footer');
 	}
+
+	public function detail($id) {
+		$data['judul'] = 'Detail Mahasiswa';
+		$data['mhs'] = $this->model('Mahasiswa_model')->getMahasiswaById($id);
+		$this->view('templates/header', $data);
+		$this->view('mahasiswa/detail', $data);
+		$this->view('templates/footer');
+	}
 }
+
+ ?>
